87678521d00b5c73db5392f2f296987397af3773
Didier <didier_75@hotmail.com>
2021-01-07 21:34:37 -0500

    quick changes

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-/*
-after checking the for prefers-reduced-motion media query
 
-hide **and then** show the SVG elements behind the logo
-*/
-const { matches } = window.matchMedia("prefers-reduced-motion: reduce");
 
-if (!matches) {
-	const text = document.querySelector("svg text");
-	// it seems it is necessary to reiterate the textLength attribute of the parent text
-	const textLength = text.getAttribute("textLength");
-	text.innerHTML = text.textContent
-		.split("")
-		.map((letter) => `<tspan textLength="${textLength}">${letter}</tspan>`)
-		.join("");
+  window.onload = function () {
+    Particles.init({
+      selector: '.background',
+      maxParticles: 450,
+      color: "#FFFFFF",
+	  connectParticles: true,
+	  retina_detect: true,
+    });
+  };
 
-	const animation = anime.timeline();
 
-	animation.add({
-		targets: "text tspan",
-		opacity: [0, 1],
-		duration: 425,
-		easing: "easeInOutQuad",
-		delay: (d, i) => 150 * i + 500
-	});
-
-	animation.add(
-		{
-			targets: "mask circle",
-			scale: [1, 0],
-			duration: 2000,
-			easing: "easeInOutSine"
-		},
-		500
-	);
-
-	window.addEventListener("click", () => animation.restart());
-}
+// particlesJS.load('particles-js', 'particlesjs.json', function() {
+// 	console.log('callback - particles.js config loaded');
+//   });	
