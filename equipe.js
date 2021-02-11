53c4831a62ce8b474554d30fad8c044d1afd1984
Didier <didier_75@hotmail.com>
2021-02-11 15:44:18 -0500

    new color

--- equipe.js
+++ equipe.js
@@ -2,7 +2,7 @@ window.onload = function () {
     Particles.init({
       selector: '.backgroundequipe',
       maxParticles: 450,
-      color: "#8055ed",
+      color: "#9ecadb",
 	  connectParticles: true,
 	  retina_detect: true,
     });
