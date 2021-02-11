1471c54cbe2882cd201fc6c4839889dd1c375447
Didier <didier_75@hotmail.com>
2021-02-11 13:49:31 -0500

    ajout de pages

---
+++ equipe.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    Particles.init({
+      selector: '.backgroundequipe',
+      maxParticles: 450,
+      color: "#FFFFFF",
+	  connectParticles: true,
+	  retina_detect: true,
+    });
+  };
--- index.js
+++ index.js
@@ -10,7 +10,6 @@
     });
   };
 
-
 // particlesJS.load('particles-js', 'particlesjs.json', function() {
 // 	console.log('callback - particles.js config loaded');
 //   });	
